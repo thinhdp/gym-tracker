12337c88b3863ed22caf9df9adce60b8ec094c97
thinhdp <thinhdp@users.noreply.github.com>
2025-08-20 16:36:06 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: "0.75rem",
+        "2xl": "1rem",
+      },
+      boxShadow: {
+        sm: "0 1px 2px 0 rgb(0 0 0 / 0.05), 0 1px 3px 0 rgb(0 0 0 / 0.08)",
+      }
+    },
+  },
+  plugins: [],
+};
