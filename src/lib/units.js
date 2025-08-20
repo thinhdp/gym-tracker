bada080fdd520b71d321708ad9d1c1b60a61029b
thinhdp <thinhdp@users.noreply.github.com>
2025-08-20 18:28:28 +0800

    Create units.js

---
+++ src/lib/units.js
@@ -0,0 +1,7 @@
+export const KG_PER_LB = 0.45359237;
+
+export const toDisplayWeight = (kg, unit) =>
+  unit === "lb" ? Math.round((Number(kg || 0) / KG_PER_LB) * 100) / 100 : Number(kg || 0);
+
+export const fromDisplayWeight = (val, unit) =>
+  unit === "lb" ? Number(val || 0) * KG_PER_LB : Number(val || 0);
