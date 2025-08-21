9478aa8339d9aac409894cf06bb93c02d6bece74
thinhdp <thinhdp@users.noreply.github.com>
2025-08-21 15:17:57 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ConfirmProvider } from "./components/ConfirmDialog";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <ConfirmProvider>
+      <App />
+    </ConfirmProvider>
+  </React.StrictMode>
 );
