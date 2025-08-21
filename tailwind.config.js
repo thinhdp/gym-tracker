7902f8e721cafb0f70ead3603316799bd456483e
thinhdp <thinhdp@users.noreply.github.com>
2025-08-21 14:17:42 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      borderRadius: {
-        xl: "0.75rem",
-        "2xl": "1rem",
-      },
-      boxShadow: {
-        sm: "0 1px 2px 0 rgb(0 0 0 / 0.05), 0 1px 3px 0 rgb(0 0 0 / 0.08)",
-      }
-    },
+      borderRadius: { xl: "0.75rem", "2xl": "1rem" },
+      boxShadow: { sm: "0 1px 2px rgb(0 0 0 / 0.05), 0 1px 3px rgb(0 0 0 / 0.08)" }
+    }
   },
-  plugins: [],
+  plugins: []
 };
